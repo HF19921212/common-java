ac2cd3c1a8a2741006acaed37188f18b51885ae2
HF19921212 <hf13216163956@aliyun.com>
2020-05-13 19:00:35 +0800

    java 基础练习

---
+++ city-brain-java/src/main/java/com/city/common/CityBrainJavaApplication.java
@@ -0,0 +1,13 @@
+package com.city.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBrainJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityBrainJavaApplication.class, args);
+    }
+
+}
---
+++ city-brain-java/src/test/java/com/city/common/CityBrainJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBrainJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
