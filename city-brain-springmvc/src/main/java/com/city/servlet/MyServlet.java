8047fd5e0a110aac39d12a3f63fb1d84ad769eb0
HF19921212 <hf13216163956@aliyun.com>
2020-05-20 15:36:35 +0800

    springmvc 链路分析

---
+++ city-brain-springmvc/src/main/java/com/city/servlet/MyServlet.java
@@ -0,0 +1,48 @@
+package com.city.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * servlet
+ * 实例化(Instance) - 初始化(init) - 调用service方法 - 销毁(destroy)
+ */
+@WebServlet(name ="MyServlet" ,urlPatterns = "/my")
+public class MyServlet extends HttpServlet {
+
+    public MyServlet(){
+        System.out.println("MyServlet -> Instance");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("MyServlet -> init");
+        super.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MyServlet -> service");
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("MyServlet -> destroy");
+    }
+}
