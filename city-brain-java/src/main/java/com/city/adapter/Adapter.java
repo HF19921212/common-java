258275fe012a48641e953060cb7a1ec323329260
HF19921212 <hf13216163956@aliyun.com>
2020-05-27 21:13:54 +0800

    适配器模式实现 类适配器方式

--- city-brain-java/src/main/java/com/city/adapter/Adapter.java
+++ city-brain-java/src/main/java/com/city/adapter/Adapter.java
@@ -1,20 +1,27 @@
 package com.city.adapter;
 
 //适配器
-public class Adapter implements Target{
+public class Adapter extends Adaptee implements Target{
 
-    private Adaptee adaptee;
+    /** 对象适配器 方式 **/
+    /*private Adaptee adaptee;
 
     public  Adapter(Adaptee adaptee){
         this.adaptee = adaptee;
-    }
+    }*/
 
     @Override
     public void createNode(int node) {
-        if(node % 2 == 0){
+        /*if(node % 2 == 0){
             adaptee.createParallelNode();
         }else{
             adaptee.createSerialNode();
+        }*/
+        /** 类适配器模式 **/
+        if(node % 2 == 0){
+            createParallelNode();
+        }else{
+            createSerialNode();
         }
     }
 
