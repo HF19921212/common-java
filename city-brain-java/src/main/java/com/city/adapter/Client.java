a5e551f114732429f6fb8c569a670c0744bf6a68
HF19921212 <hf13216163956@aliyun.com>
2020-05-27 21:17:48 +0800

    适配器模式实现 类适配器方式

--- city-brain-java/src/main/java/com/city/adapter/Client.java
+++ city-brain-java/src/main/java/com/city/adapter/Client.java
@@ -2,8 +2,9 @@ package com.city.adapter;
 
 public class Client {
     public static void main(String[] args) {
-        Adaptee adaptee = new Adaptee();
-        Target target = new Adapter(adaptee);
+        //Adaptee adaptee = new Adaptee();
+        //Target target = new Adapter(adaptee);
+        Target target = new Adapter();
         target.createNode(1);
     }
 }
