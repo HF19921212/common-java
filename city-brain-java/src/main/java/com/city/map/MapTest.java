1aca581bb9378596cf2e1b51a7d84c0bc2d2362d
HF19921212 <hf13216163956@aliyun.com>
2020-06-01 00:27:54 +0800

    HashMap 源码解析

---
+++ city-brain-java/src/main/java/com/city/map/MapTest.java
@@ -0,0 +1,13 @@
+package com.city.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap(13);
+        map.put(1,"hefan");
+    }
+
+}
