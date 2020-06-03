a0ffac67278554371ae181555d0207530717704b
HF19921212 <hf13216163956@aliyun.com>
2020-06-03 10:27:12 +0800

    提交遗漏代码

--- city-brain-springmvc/src/main/test/BeanTest.java
+++ city-brain-springmvc/src/main/test/BeanTest.java
@@ -1,12 +1,21 @@
 import com.city.domain.Account;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BeanTest {
 
     public static void main(String[] args) {
+        /**
+         * scan 扫描我们的类,将类进行解析成 BeanDefinition 类描述对象
+         */
         AnnotationConfigApplicationContext act = new AnnotationConfigApplicationContext(Account.class);
         Account account = (Account) act.getBean("account");
         System.out.println(account.getSex());
+
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring-bean.xml");
+        //Account account = (Account) context.getBean("account");
+        //System.out.println(account.getSex());
     }
 
 }
