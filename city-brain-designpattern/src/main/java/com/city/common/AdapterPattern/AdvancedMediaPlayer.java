85b04dae114a4237218693d043728c12983d1abe
HF19921212 <hf13216163956@aliyun.com>
2020-06-23 17:37:00 +0800

    设计模式 适配器模式

---
+++ city-brain-designpattern/src/main/java/com/city/common/AdapterPattern/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package com.city.common.AdapterPattern;
+
+//高级媒体播放器
+public interface AdvancedMediaPlayer {
+    void playVlc(String fileName);
+    void playMp4(String fileName);
+}
---
+++ city-brain-designpattern/src/main/java/com/city/common/AdapterPattern/MediaPlayer.java
@@ -0,0 +1,6 @@
+package com.city.common.AdapterPattern;
+
+//媒体播放
+public interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
