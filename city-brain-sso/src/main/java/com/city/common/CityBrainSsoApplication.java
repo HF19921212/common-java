22df4bcf69d6e77817e1751addb9e4e3a3241338
HF19921212 <hf13216163956@aliyun.com>
2020-06-25 17:02:08 +0800

    springdata jpa + redis 实现单点登录

---
+++ city-brain-sso/src/main/java/com/city/common/CityBrainSsoApplication.java
@@ -0,0 +1,13 @@
+package com.city.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBrainSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityBrainSsoApplication.class, args);
+    }
+
+}
---
+++ city-brain-sso/src/main/java/com/city/common/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.city.common.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.city.common.pojo.ItdragonResult;
+import com.city.common.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 登录
+     * @param username
+     * @param password
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    @ResponseBody
+    public ItdragonResult userLogin(String username, String password,
+                                    HttpServletRequest request, HttpServletResponse response) {
+        try {
+            ItdragonResult result = userService.userLogin(username, password, request, response);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ItdragonResult.build(500, "");
+        }
+    }
+
+    /**
+     * 登出
+     * @param token
+     * @return
+     */
+    @RequestMapping(value="/logout")
+    @ResponseBody
+    public ItdragonResult logout(String token) {
+        userService.logout(token); // 思路是从Redis中删除key，实际情况请和业务逻辑结合
+        return ItdragonResult.ok();
+    }
+
+    /**
+     * 获取登录用户信息
+     * @param token
+     * @return
+     */
+    @RequestMapping("/token")
+    @ResponseBody
+    public Object getUserByToken(String token) {
+        ItdragonResult result = null;
+        try {
+            result = userService.queryUserByToken(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = ItdragonResult.build(500, "");
+        }
+        return result;
+    }
+}
